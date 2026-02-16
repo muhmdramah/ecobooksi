61373c5d17288c57ffe4d80e2756959cb95b1b84
muhmdramah <mohammedrammahux@gmail.com>
2026-02-16 17:05:49 +0200

    Great! Here's a professional commit message for this change:
    ```
    refactor: remove redundant DbSet field from GenericRepository
    
    - Eliminate unnecessary _dbSet field that duplicates _context.Set<T>()
    - Use _context.Set<T>() directly in all repository methods
    - Remove commented line about Categories/DbSet relationship
    - Simplify code while maintaining identical functionality
    
    This change reduces code complexity and eliminates the need to maintain
    a separate DbSet reference that was only used to call _context.Set<T>()
    internally.

--- ecobooksi.DataAccess/Repositories/GenericRepository.cs
+++ ecobooksi.DataAccess/Repositories/GenericRepository.cs
@@ -8,25 +8,21 @@ namespace ecobooksi.DataAccess.Repositories
     public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
         private readonly ApplicationDbContext _context;
-        internal DbSet<T> _dbSet;
-
         public GenericRepository(ApplicationDbContext context)
         {
             _context = context;
-            _dbSet = _context.Set<T>();
-            //means _context.Categories == _dbSet;
         }
 
         public async Task<ICollection<T>> GetAllAsync()
         {
-            IQueryable<T> query = _dbSet;
+            IQueryable<T> query = _context.Set<T>();
 
             return await query.ToListAsync();
         }
 
         public async Task<T> GetAsync(Expression<Func<T, bool>> filter)
         {
-            IQueryable<T> query = _dbSet;
+            IQueryable<T> query = _context.Set<T>();
             query = query.Where(filter);
 
             return await query.FirstOrDefaultAsync();
@@ -34,19 +30,19 @@ namespace ecobooksi.DataAccess.Repositories
 
         public async Task Create(T entity)
         {
-            await _dbSet.AddAsync(entity);
+            await _context.Set<T>().AddAsync(entity);
             _context.SaveChanges();
         }
 
         public async Task Delete(T entity)
         {
-            _dbSet.Remove(entity);
+            _context.Set<T>().Remove(entity);
             _context.SaveChanges();
         }
 
         public async Task DeleteRange(IEnumerable<T> entities)
         {
-            _dbSet.RemoveRange(entities);
+            _context.Set<T>().RemoveRange(entities);
             _context.SaveChanges();
         }
     }
