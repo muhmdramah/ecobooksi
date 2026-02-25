444f1091ba9838dfa627af90fffc7d924ffa99a6
muhmdramah <mohammedrammahux@gmail.com>
2026-02-25 17:31:32 +0200

    feat(auth): update RegisterModel to work with ApplicationUser
    - Change CreateUser() to return ApplicationUser instead of IdentityUser
    - Add using for ecobooksi.Models.Models.Auth namespace
    - Maintain null check for IEmailSender

--- ecobooksiWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ecobooksiWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
+using ecobooksi.Models.Models.Auth;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -157,16 +158,16 @@ namespace ecobooksi.Web.Areas.Identity.Pages.Account
             return Page();
         }
 
-        private IdentityUser CreateUser()
+        private ApplicationUser CreateUser()
         {
             try
             {
-                return Activator.CreateInstance<IdentityUser>();
+                return Activator.CreateInstance<ApplicationUser>();
             }
             catch
             {
-                throw new InvalidOperationException($"Can't create an instance of '{nameof(IdentityUser)}'. " +
-                    $"Ensure that '{nameof(IdentityUser)}' is not an abstract class and has a parameterless constructor, or alternatively " +
+                throw new InvalidOperationException($"Can't create an instance of '{nameof(ApplicationUser)}'. " +
+                    $"Ensure that '{nameof(ApplicationUser)}' is not an abstract class and has a parameterless constructor, or alternatively " +
                     $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
             }
         }
