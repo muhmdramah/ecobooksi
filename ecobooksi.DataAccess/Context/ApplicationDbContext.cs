bc3f1af18dec71aca668e00d043e13587b129ea8
muhmdramah <mohammedrammahux@gmail.com>
2026-03-01 03:31:20 +0200

    Feat: Add order management models and database tables
    - Create OrderHeader model for order master data with user details and payment info
    - Create OrderDetail model for order line items with product references
    - Add migration to create OrderHeaders and OrderDetails tables
    - Establish foreign key relationships between orders, users, and products

--- ecobooksi.DataAccess/Context/ApplicationDbContext.cs
+++ ecobooksi.DataAccess/Context/ApplicationDbContext.cs
@@ -123,5 +123,7 @@ namespace ecobooksi.DataAccess.Context
         public DbSet<Company> Companies { get; set; }
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
     }
 }
---
+++ ecobooksi.Models/Models/OrderHeader.cs
@@ -0,0 +1,52 @@
+﻿using ecobooksi.Models.Models.Auth;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ecobooksi.Models.Models
+{
+    public class OrderHeader
+    {
+        public int OrderHeaderId { get; set; }
+
+
+        [ForeignKey(nameof(ApplicationUser))]
+        public string ApplicationUserId { get; set; }
+        [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public DateTime OrderDate { get; set; }
+        public DateTime ShippingDate { get; set; }
+        public double OrderTotal { get; set; }
+
+
+        public string? OrderStatus { get; set; }
+        public string? PaymentStatus { get; set; }
+        public string? TrackingNumber { get; set; }
+        public string? Carrier { get; set; }
+
+        public DateTime PaymentDate { get; set; }
+        public DateOnly PaymentDueDate { get; set; }
+
+        public string? PaymentIntentId { get; set; }
+
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string StreetAddress { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public string PostalCode { get; set; }
+    }
+}
